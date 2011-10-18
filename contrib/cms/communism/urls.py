9c2b8651e8091ff6a3b7fd63a68101da64f2845a
Joey Leingang <leingang@arizona.edu>
2011-10-18 17:17:57 +0000

    scampi genericizing

--- contrib/cms/communism/urls.py
+++ contrib/cms/communism/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
 from django.views.decorators.cache import cache_page
-from libscampi.contrib.cms.communism.views import story_detail, story_archive_day, story_archive_month, story_archive_year, story_archive, view_commune, primary_section
+
+from libscampi.contrib.cms.communism.views import view_commune, primary_section
+#from libscampi.contrib.cms.communism.views import story_detail, story_archive_day, story_archive_month, story_archive_year, story_archive, view_commune, primary_section
 
 urlpatterns = patterns('libscampi.contrib.cms.communism.views',
-    
+    """
     url(r'^(?P<keyname>[\w\.\-]+)/(?P<publishword>[\w/-]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[\w/-]+)/$', 
         cache_page(story_detail, 60*5), name="published-story-detail"),
     url(r'^(?P<keyname>[\w\.\-]+)/(?P<publishword>[\w/-]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', 
@@ -14,7 +16,7 @@ urlpatterns = patterns('libscampi.contrib.cms.communism.views',
         cache_page(story_archive_year, 60*60), name="published-story-archive-year"),
     url(r'^(?P<keyname>[\w\.\-]+)/(?P<publishword>[\w/-]+)/$', 
         cache_page(story_archive, 60*60), name="published-story-archive"),
-            
+    """        
     url(r'^(?P<keyname>[\w\.\-]+)/$', view_commune, name="view-commune"),
     url(r'^$', primary_section, name="primary-section"),
 )
--- contrib/cms/communism/views/__init__.py
+++ contrib/cms/communism/views/__init__.py
@@ -1,4 +1,4 @@
-from .functional import story_archive, story_archive_year, story_archive_month, story_archive_day, story_detail
+#from .functional import story_archive, story_archive_year, story_archive_month, story_archive_day, story_detail
 from .base import Index
 
 view_commune = Index.as_view()

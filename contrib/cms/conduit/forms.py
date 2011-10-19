be112bc1d3dade43ded8bd0c453d6788ff4088a5
Joey Leingang <leingang@arizona.edu>
2011-10-19 18:25:21 +0000

    making dynamic pickers better

--- contrib/cms/conduit/forms.py
+++ contrib/cms/conduit/forms.py
@@ -5,6 +5,6 @@ from libscampi.contrib.cms.conduit.picker import manifest
 from libscampi.contrib.cms.conduit.models import DynamicPicker
 
 class DynamicPickerForm(forms.ModelForm):
-    content = forms.ModelChoiceField(queryset = manifest.contenttypes_for_available())
+    content = forms.ModelChoiceField(queryset = manifest.contenttypes_for_available(), help_text = _("What model will populate this picker?"))
     class Meta:
         model = DynamicPicker

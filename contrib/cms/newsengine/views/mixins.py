e5d6a84a15f72debc559dd5404ec949b34338e64
Joey Leingang <leingang@arizona.edu>
2011-10-21 18:46:57 +0000

    story archives and better dynamic pickers

--- contrib/cms/newsengine/views/mixins.py
+++ contrib/cms/newsengine/views/mixins.py
@@ -1,9 +1,10 @@
 from libscampi.contrib.cms.newsengine.models import Publish
 
 class PublishStoryMixin(object):
-    queryset = Publish.objects.all()
+    #queryset = Publish.objects.all()
 
     model = Publish
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
+    date_field = 'start'
     

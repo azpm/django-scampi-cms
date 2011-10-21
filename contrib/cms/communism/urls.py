1c4ccbd7b39bfc249da02f44cac8e2009e01cd5e
Joey Leingang <leingang@arizona.edu>
2011-10-21 22:08:52 +0000

    story archives and better dynamic pickers

--- contrib/cms/communism/urls.py
+++ contrib/cms/communism/urls.py
@@ -5,7 +5,7 @@ from libscampi.contrib.cms.communism.views import view_commune, primary_section
 from libscampi.contrib.cms.newsengine.urls import urlpatterns as newsengine_archive_urls
 
 urlpatterns = patterns('libscampi.contrib.cms.communism.views',
-    url(r'^(?P<keyname>[\w\.\-]+)/(?P<picker>[\w/-]+)/', include(newsengine_archive_urls)),
+    url(r'^(?P<keyname>[\w\.\-]+)/(?P<picker>[w-]+)/', include(newsengine_archive_urls)),
     url(r'^(?P<keyname>[\w\.\-]+)/$', view_commune, name="view-commune"),
     url(r'^$', primary_section, name="primary-section"),
 )

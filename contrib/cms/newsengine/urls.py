e36c04f08e40dbd013417e0fc28171f7ee88c98c
Joey Leingang <leingang@arizona.edu>
2011-10-21 22:10:19 +0000

    story archives and better dynamic pickers

--- contrib/cms/newsengine/urls.py
+++ contrib/cms/newsengine/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('libscampi.contrib.cms.communism.views',
     url(r'^(?P<year>\d{4})/$', story_archive_year, name="published-story-archive-year"),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', story_archive_month, name="published-story-archive-month"),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', story_archive_day, name="published-story-archive-day"),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[w-]+)/$',  story_detail, name="published-story-detail"),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[\w-]+)/$',  story_detail, name="published-story-detail"),
 )

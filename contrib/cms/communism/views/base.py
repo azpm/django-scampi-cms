401c8530df3e2e710005bfd9aa3bb30eaa483ca9
Joey Leingang <leingang@arizona.edu>
2011-10-26 22:21:45 +0000

    creating admin documentation

--- contrib/cms/communism/views/base.py
+++ contrib/cms/communism/views/base.py
@@ -1,4 +1,19 @@
 from libscampi.contrib.cms.views.base import Page
 
 class Index(Page):
-    pass
+    """
+    Index/Commune landing page for Scampi CMS
+    
+    Index inherits from: libscamp.contrib.cms.views.base.Page which in turn inherits from:
+    
+    1. libscampi.contrib.cms.communism.views.mixins.CommuneMixin
+    2. libscampi.contrib.cms.views.mixins.PageMixin
+    3. libscampi.contrib.cms.communism.views.mixins.ThemeMixin
+    4. libscampi.contrib.cms.communism.views.mixins.CSSMixin
+    5. libscampi.contrib.cms.communism.views.mixins.JScriptMixin
+    
+    CommuneMixin provides several instance members:
+        commune - :model:`communism.Commune`
+        section - :model:`communism.Section`
+        realm - :model:`communism.Realm`
+    """

3a88c2960b06817cdff1129f4c2c285915147fa3
Joey Leingang <leingang@arizona.edu>
2011-11-04 22:45:15 +0000

    trying out logging

--- contrib/cms/communism/models.py
+++ contrib/cms/communism/models.py
@@ -131,7 +131,8 @@ class Realm(models.Model):
     def _primary_section(self):
         "Returns the first active section for this realm, or None"
         try:
-            t = self.section_set.filter(active = True, extends = None).order_by('display_order')[0]
+            t = Section.objects.get(active = True, extends = None, section__id = seld.id)
+            #t = self.section_set.filter(active = True, extends = None).order_by('display_order')[0]
         except IndexError:
             t = None
         return t

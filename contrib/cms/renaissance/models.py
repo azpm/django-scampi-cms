932e8f122408f6a39a5e29589bc042efc1b4775e
Joey Leingang <leingang@arizona.edu>
2011-11-10 22:50:56 +0000

    bug fixing

--- contrib/cms/renaissance/models.py
+++ contrib/cms/renaissance/models.py
@@ -193,7 +193,7 @@ def _file_upload_pathing(cls, fname):
     #fname = re.sub(r'\s','', fname) #strip all whitespace chars from file
     name = slugify(file)
     
-    return "master/%s/%d/%d/%d/%s/%s.%s" % (cls.base_type, now.year, now.month, now.day, cls.type.keyname, name, ext)
+    return "master/%s/%d/%d/%d/%s/%s%s" % (cls.base_type, now.year, now.month, now.day, cls.type.keyname, name, ext)
 
 class Image(Media):
     file = models.ImageField(upload_to=_file_upload_pathing, validators=[ValidImgExtension()])

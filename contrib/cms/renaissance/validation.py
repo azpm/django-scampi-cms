9742a8066a9a996381b675bdbacbe66ae4be1170
Joey Leingang <leingang@arizona.edu>
2011-11-10 21:33:06 +0000

    bug fixing

--- contrib/cms/renaissance/validation.py
+++ contrib/cms/renaissance/validation.py
@@ -1,6 +1,12 @@
+import os
 from django.core import validators
 from django.core.exceptions import ValidationError
 
 def has_extension(value):
-    f = value.name
-    assert False
+    ext = os.path.splitext(value.name)[1]
+    
+    if not ext:
+        raise ValidationError(u"File (%s) must have extension" % value.name)
+    
+    
+    raise ValidationError(u"fake File (%s) must have extension" % value.name)

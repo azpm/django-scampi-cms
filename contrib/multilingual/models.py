d996e4b831ca82e9780fd48f86ff690b8f84ea6c
Joey Leingang <leingang@arizona.edu>
2011-11-21 18:32:57 +0000

    multilingual performance

--- contrib/multilingual/models.py
+++ contrib/multilingual/models.py
@@ -92,11 +92,11 @@ class MultilingualModel(models.Model):
                 translation = self.__dict__.get(translation_key, None)
                 if not translation:
                     try:
-                        translation = self._meta.translation.objects.get(model=self, language__code=code).values()
+                        translation = self._meta.translation.objects.get(model=self, language__code=code)
                     except ObjectDoesNotExist:
                         if MULTILINGUAL_FALL_BACK_TO_DEFAULT and MULTILINGUAL_DEFAULT and code != MULTILINGUAL_DEFAULT:
                             try:
-                                translation = self._meta.translation.objects.get(model=self, language__code=MULTILINGUAL_DEFAULT).values()
+                                translation = self._meta.translation.objects.get(model=self, language__code=MULTILINGUAL_DEFAULT)
                             except ObjectDoesNotExist:
                                 if not MULTILINGUAL_FAIL_SILENTLY:
                                     raise ValueError, "'%s' has no translation in '%s'"%(self, code)
@@ -108,7 +108,7 @@ class MultilingualModel(models.Model):
                         assert False
                         self.__dict__[translation_key] = translation
                 
-                return translation[field]
+                return translation.__dict__[field]
                 
         raise AttributeError, "'%s' object has no attribute '%s'"%(self.__class__.__name__, str(attr))
     

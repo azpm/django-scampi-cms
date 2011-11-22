298132a8cd5783a408e409502f7df7ddfad07918
Joey Leingang <leingang@arizona.edu>
2011-11-22 17:12:10 +0000

    performance tuning newsengine admin

--- contrib/cms/newsengine/admin.py
+++ contrib/cms/newsengine/admin.py
@@ -87,7 +87,7 @@ class StoryAdmin(admin.ModelAdmin):
     filter_horizontal = ['categories']
     
     def headline(self, cls):
-        return u"%s" % cls.article.headline_en
+        return u"%s" % cls.article.headline
     
     def queryset(self, request):
         qs = super(StoryAdmin, self).queryset(request)

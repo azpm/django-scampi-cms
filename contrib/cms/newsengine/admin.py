86162bd716fdd925c8a1c545deaf54cbff502047
Joey Leingang <leingang@arizona.edu>
2011-11-22 17:10:50 +0000

    performance tuning newsengine admin

--- contrib/cms/newsengine/admin.py
+++ contrib/cms/newsengine/admin.py
@@ -92,7 +92,7 @@ class StoryAdmin(admin.ModelAdmin):
     def queryset(self, request):
         qs = super(StoryAdmin, self).queryset(request)
         
-        return qs.select_related('author', 'article__articletranslation__headline').filter(article__articletranslation__language__code = 'en')
+        return qs.select_related('author', 'article__translations__headline').filter(article__translations__headline__language__code = 'en')
     
 
     def save_model(self, request, obj, form, change):

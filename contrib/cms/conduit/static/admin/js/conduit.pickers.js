f3f384b95a04bd90d9c6f5d01615acf654228da0
Joey Leingang <leingang@arizona.edu>
2011-11-29 21:16:50 +0000

    making pickers actually work

--- contrib/cms/conduit/static/admin/js/conduit.pickers.js
+++ contrib/cms/conduit/static/admin/js/conduit.pickers.js
@@ -121,7 +121,7 @@ Pickers.prototype.add_filter = function(type, group_suffix)
     jQuery("#"+id_pointer+"_filters > div.form-row").filter(":last").after('\
  		<div class="form-row" data-filter-ptr="'+index+'">\
  			<div>\
- 				<label>'+picking_filter.name+'</label>'+picking_filter.html+'<a class="deletelink"></a> \
+ 				<label>'+picking_filter.name+'</label>'+picking_filter.html+'&nbsp;<a class="deletelink"></a> \
  			</div> \
  		</div>');
  	

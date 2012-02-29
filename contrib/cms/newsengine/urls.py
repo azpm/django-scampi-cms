ac21a7e01c298c845babe318b1366e812f558613
Joey Leingang <leingang@arizona.edu>
2012-02-29 21:41:21 +0000

    making news engine archives pretty

--- contrib/cms/newsengine/urls.py
+++ contrib/cms/newsengine/urls.py
@@ -4,9 +4,9 @@ from django.views.decorators.cache import cache_page
 from libscampi.contrib.cms.newsengine.views import story_archive, story_detail, story_archive_day, story_archive_month, story_archive_year
 
 urlpatterns = patterns('libscampi.contrib.cms.communism.views',
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[\w-]+)/$',  cache_page(story_detail, 60 * 10), name="published-story-detail"), 
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', cache_page(story_archive_day, 60 * 10), name="published-story-archive-day"), 
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', cache_page(story_archive_month, 60 * 20), name="published-story-archive-month"),
-    url(r'^(?P<year>\d{4})/$', cache_page(story_archive_year, 60*25), name="published-story-archive-year"),
-    url(r'^$', cache_page(story_archive, 60*60), name="published-story-archive"),    
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[\w-]+)/$',  cache_page(story_detail, 60 * 10), name="pubstory-detail"), 
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', cache_page(story_archive_day, 60 * 10), name="pubstory-archive-day"), 
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', cache_page(story_archive_month, 60 * 20), name="pubstory-archive-month"),
+    url(r'^(?P<year>\d{4})/$', cache_page(story_archive_year, 60*25), name="pubstory-archive-year"),
+    url(r'^$', cache_page(story_archive, 60*60), name="pubstory-archive"),    
 )

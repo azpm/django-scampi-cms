aaaa2986cb4d356e799399db812cb7179b33334c
Joey Leingang <leingang@arizona.edu>
2012-03-19 18:31:17 +0000

    article previews

--- contrib/cms/newsengine/admin.py
+++ contrib/cms/newsengine/admin.py
@@ -20,7 +20,7 @@ class ArticleTranslationInline(admin.StackedInline):
         ('Discourse', {'fields': ('headline', 'sub_headline', 'body')}),
         ('Dialect', {'fields': ['language']}),
     )
-    extra = 0
+    extra = 1
     form = ArticleTranslationForm
 
 class ArticleAdmin(admin.ModelAdmin):

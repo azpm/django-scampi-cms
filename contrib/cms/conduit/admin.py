33192e4a683cc766cea360e7b12a28f519bc9885
Joey Leingang <leingang@arizona.edu>
2012-03-22 20:50:40 +0000

    dynamic picker admin betterness

--- contrib/cms/conduit/admin.py
+++ contrib/cms/conduit/admin.py
@@ -65,6 +65,9 @@ class DynamicPickerAdmin(admin.ModelAdmin):
             'https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js',
             'http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js',
             'admin/js/conduit.pickers.js',
+            "admin/js/core.js",
+            "admin/js/SelectBox.js",
+            "admin/js/SelectFilter2.js"
         )
 
     #we have the "adding a picker" form and the "changing the picker" form

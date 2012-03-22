5770aaff624b896eb8844deeb83d545fad149c83
Joey Leingang <leingang@arizona.edu>
2012-03-22 18:37:47 +0000

    dynamic picker admin betterness

--- contrib/cms/newsengine/models.py
+++ contrib/cms/newsengine/models.py
@@ -179,8 +179,7 @@ class PublishInlineMediaOverride(models.Model):
 class PublishPicking(django_filters.FilterSet):
     start = django_filters.filters.DateRangeFilter(lookup_type=('lt','gt','lte','gte'))
     end = django_filters.filters.DateRangeFilter(name="end", lookup_type=('lt','gt','lte','gte'))
-    categories = django_filters.filters.ModelMultipleChoiceFilter(name="story__categories", widget=FilteredSelectMultiple("Subjects",False))
-    
+    categories = django_filters.filters.ModelMultipleChoiceFilter(name="story__categories")    
     
     class Meta:
         model = Publish

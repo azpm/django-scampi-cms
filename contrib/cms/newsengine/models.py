233930dc8e6189f79b3cedd6b5a8e52c58396e4a
Joey Leingang <leingang@arizona.edu>
2012-03-28 18:18:05 +0000

    prefetch_related optimizations

--- contrib/cms/newsengine/models.py
+++ contrib/cms/newsengine/models.py
@@ -208,7 +208,7 @@ class PublishPicking(django_filters.FilterSet):
     def static_prefetch_related():
         return (
             'story__article',
-            'story__article__video_inlines'
+            'story__article__translations',
         )
         
     @staticmethod

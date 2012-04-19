874477f58fe70ebea0dc776df3af84477a9061e2
Joey Leingang <leingang@arizona.edu>
2012-04-19 00:25:08 +0000

    picker performance

--- contrib/cms/conduit/models.py
+++ contrib/cms/conduit/models.py
@@ -130,7 +130,7 @@ class DynamicPicker(PickerBase):
             
         #limit the qs if necessary
         if self.max_count > 0:
-            cache.set(cache_key, qs[:self.max_count].values_list('id', flat=True), 60*10)
+            cache.set(cache_key, qs.values_list('id', flat=True)[:self.max_count], 60*10)
             return qs[:self.max_count]
 
         cache.set(cache_key, qs.values_list('id', flat=True), 60*10)

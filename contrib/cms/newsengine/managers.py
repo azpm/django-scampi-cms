550ddc540b203936c69077e4b338fc1ff9df9044
Joey Leingang <leingang@arizona.edu>
2012-06-16 01:40:30 +0000

    related stories

--- contrib/cms/newsengine/managers.py
+++ contrib/cms/newsengine/managers.py
@@ -12,8 +12,8 @@ class PublishedManager(models.Manager):
         right_now = datetime.now()
         long_ago = right_now - timedelta(days=30)
 
-        qs = self.get_query_set().filter(Q(story__peers__in=[story]) | Q(story__categories__keyname__in=cats), start__lte=right_now, start__gte=long_ago).exclude(story__id=story.pk)
-        qs = qs.annotate(rel_count=Avg('story__categories'))
+        qs = self.get_query_set().filter(Q(story__peers__in=[story]) | Q(story__categories__keyname__in=cats), start__lte=right_now, start__gte=long_ago).exclude(story__id=story.pk).distinct('story')
+        qs = qs.annotate(rel_count=Count('story__categories'))
 
         return qs.order_by('rel_count')
 

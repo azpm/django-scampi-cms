547af1fab944558294a57d3017f46e6bcc00495c
Joey Leingang <leingang@arizona.edu>
2012-06-16 01:39:30 +0000

    related stories

--- contrib/cms/newsengine/managers.py
+++ contrib/cms/newsengine/managers.py
@@ -15,7 +15,7 @@ class PublishedManager(models.Manager):
         qs = self.get_query_set().filter(Q(story__peers__in=[story]) | Q(story__categories__keyname__in=cats), start__lte=right_now, start__gte=long_ago).exclude(story__id=story.pk)
         qs = qs.annotate(rel_count=Avg('story__categories'))
 
-        return qs.order_by('-rel_count')
+        return qs.order_by('rel_count')
 
 
 class CategoryGenera(models.Manager):

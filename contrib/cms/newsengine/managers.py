80af130110762568b946b10bf140ee28ccbd1d51
Joey Leingang <leingang@arizona.edu>
2012-06-16 01:29:31 +0000

    related stories

--- contrib/cms/newsengine/managers.py
+++ contrib/cms/newsengine/managers.py
@@ -13,9 +13,9 @@ class PublishedManager(models.Manager):
         long_ago = right_now - timedelta(days=30)
 
         qs = self.get_query_set().filter(Q(story__peers__in=[story]) | Q(story__categories__keyname__in=cats), start__lte=right_now, start__gte=long_ago).exclude(story__id=story.pk)
-        qs = qs.annotate(rel_count=StdDev('story__categories'))
+        qs = qs.annotate(rel_count=Variance('story__categories__keyname'))
 
-        return qs.order_by('rel_count')
+        return qs.order_by('-rel_count')
 
 
 class CategoryGenera(models.Manager):

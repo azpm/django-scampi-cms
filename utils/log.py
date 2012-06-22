483639d7c865230c356832415e3e1bdd1f5bcc0c
Joey Leingang <leingang@arizona.edu>
2012-06-22 18:17:10 +0000

    logging fix

--- utils/log.py
+++ utils/log.py
@@ -3,4 +3,6 @@ from django.conf import settings
 
 class RequireDebugTrue(logging.Filter):
     def filter(self, record):
-        return record.level > 10 or settings.DEBUG
+        if record.level > 10 or settings.DEBUG:
+            return True
+        return False

dcf4d1bd121c4cbca3de68c3d6f2757c46620262
Joey Leingang <leingang@arizona.edu>
2012-07-13 21:26:17 +0000

    noisy debug

--- utils/log.py
+++ utils/log.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 class RequireDebugTrue(logging.Filter):
     def filter(self, record):
-        if record.levelno > 10 or settings.DEBUG:
+        if record.levelno > 10 or settings.DEBUG and settings.DEBUG_NOISY:
             return True
         return False

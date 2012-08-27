872dd1b4568071073127771d6280fb1e988747f7
Joey Leingang <leingang@arizona.edu>
2012-08-27 23:55:50 +0000

    fidgeting with setup.py
    I know this is wrong -- but we need it this way for legacy deployment

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from distutils.core import setup
 setup(
     name='libscampi',
     version='4.1',
-    packages=['libscampi'],
-    package_dir={'libscampi': '.'},
     url='https://github.com/azpm/django-scampi-cms',
     license='BSD',
     author='Joey Leingang',

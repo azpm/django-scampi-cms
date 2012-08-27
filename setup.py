599bd8a3378504f22477c0f6fbb9283f8e0bc0f7
Joey Leingang <leingang@arizona.edu>
2012-08-27 23:57:03 +0000

    Re-organizing for distribution

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='libscampi',
     version='4.1',
-    packages=['contrib','core','utils'],
+    packages=['libscampi'],
     url='https://github.com/azpm/django-scampi-cms',
     license='BSD',
     author='Joey Leingang',

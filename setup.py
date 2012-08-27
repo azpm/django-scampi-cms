c0df45e7ec48945534027bcfe4ac138c9b466f5a
Joey Leingang <leingang@arizona.edu>
2012-08-27 23:55:54 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='libscampi',
     version='4.1',
-    py_modules=['contrib','core','utils'],
+    packages=['contrib','core','utils'],
     url='https://github.com/azpm/django-scampi-cms',
     license='BSD',
     author='Joey Leingang',

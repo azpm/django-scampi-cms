deb065ebe7dff6a6912e97a6e56bf7da75dae1ae
Joey Leingang <leingang@arizona.edu>
2012-08-27 23:55:44 +0000

    setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='libscampi',
+    version='4.1',
+    packages=['libscampi'],
+    package_dir={'': 'libscampi'},
+    url='https://github.com/azpm/django-scampi-cms',
+    license='BSD',
+    author='Joey Leingang',
+    author_email='jleingang@azpm.org',
+    description='django based content management system'
+)

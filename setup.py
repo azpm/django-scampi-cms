b0e2c2c6a924428f9f0f2e95aabe9700ce70f249
Joey Leingang <leingang@arizona.edu>
2012-08-29 05:37:22 +0000

    fidgeting with setup.py
    I know this isn't the right way to do this :/

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='libscampi',
     version='4.1',
     packages=['libscampi'],
-    package_dir={'': '.'},
+    package_dir={'libscampi': '.'},
     url='https://github.com/azpm/django-scampi-cms',
     package_dir={'libscampi': 'libscampi'},
     packages=packages,

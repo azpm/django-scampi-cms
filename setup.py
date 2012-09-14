6f15869fe77fd1f4739fee6e67a10ca09582ab65
multigl <leingang@arizona.edu>
2012-09-13 20:38:52 -0700

    setup should include non python files now

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
 import os
 
-from libscampi import get_version
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils not to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
 # Compile the list of packages available, because distutils doesn't have
 # an easy way to do this.
 packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
-if root_dir:
+if root_dir != '':
     os.chdir(root_dir)
 
 for dirpath, dirnames, filenames in os.walk('libscampi'):
@@ -15,24 +34,21 @@ for dirpath, dirnames, filenames in os.walk('libscampi'):
     for i, dirname in enumerate(dirnames):
         if dirname.startswith('.'): del dirnames[i]
     if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
+        packages.append('.'.join(fullsplit(dirpath)))
     elif filenames:
-        prefix = dirpath[13:] # Strip "libscampi/" or "libscampi\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+# Dynamically calculate the version based on django.VERSION.
+version = __import__('libscampi').get_version()
 
 setup(name='django-scampi-cms',
-    version=get_version().replace(' ', '-'),
+    version=version,
     description='A django content management system.',
     author='Joey Leingang',
     author_email='leingang@arizona.edu',
     url='https://github.com/azpm/django-scampi-cms',
-    package_dir={'libscampi': 'libscampi'},
-    packages=packages,
-    package_data={'libscampi': data_files},
+    packages = packages,
+    data_files = data_files,
     classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Web Environment',
                  'Framework :: Django',

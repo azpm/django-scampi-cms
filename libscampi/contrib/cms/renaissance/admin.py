1f6c931f2f4fa1b3daf623ca6b31ba42b1e7e90a
aswedbergh <aswedbergh@gmail.com>
2019-01-23 11:54:31 -0700

    Changing repro rights order

--- libscampi/contrib/cms/renaissance/admin.py
+++ libscampi/contrib/cms/renaissance/admin.py
@@ -28,7 +28,7 @@ class FileBasedMediaAdmin(MediaAdmin, admin.ModelAdmin):
     def get_list_display(self, request):
         if self.model is Image:
             if request.GET.get("pop",False):
-                return ['title', 'popover', 'reproduction_allowed']
+                return ['title', 'reproduction_allowed', 'popover' ]
             else:
                 return ['title', 'slug','popover','creation_date', 'reproduction_allowed']
         else:

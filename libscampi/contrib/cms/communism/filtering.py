a38a579f3f6f615f7f1384bdb85abfa1f61f447b
Joey Leingang <multigl@gmail.com>
2020-08-18 17:45:37 -0400

    delete scampi toolbar -- if we build one, it'll be done with react and not jquery

---
+++ libscampi/contrib/cms/communism/filtering.py
@@ -0,0 +1,19 @@
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.admin import SimpleListFilter
+
+from libscampi.contrib.cms.conduit.picker import manifest
+
+
+class ContentTypeListFilter(SimpleListFilter):
+    """
+    Filter list by available picking contenttypes
+    """
+    title = _("Content Source")
+    parameter_name = 'cs'
+
+    def lookups(self, request, model_admin):
+        return [(ct.id, ct.name) for ct in manifest.contenttypes_for_available()]
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            return queryset.filter(content_id=self.value())
--- libscampi/contrib/cms/toolbar/middleware.py
+++
@@ -1,7 +0,0 @@
-from libscampi.contrib.cms.toolbar.scampitoolbar import ScampiToolbar
-
-_HTML_TYPES = ('text/html', 'application/xhtml+xml')
-
-class ScampiToolbarMiddleware(object):
-    def process_request(self, request):
-        request.toolbar = ScampiToolbar(request)
